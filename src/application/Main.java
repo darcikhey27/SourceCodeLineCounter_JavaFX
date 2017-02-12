3009aaee719b27861c22dc7be52b037b045da671
Darci Khey Saucedo <Darci Khey Saucedo>
2017-02-12 00:38:16 -0800

    edited md file

--- src/application/Main.java
+++ src/application/Main.java
@@ -4,6 +4,7 @@ import javafx.application.Application;
 import javafx.stage.Stage;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.HBox;
 import javafx.fxml.FXMLLoader;
@@ -13,9 +14,17 @@ public class Main extends Application {
     public void start(Stage primaryStage) {
 	try {
 	    Parent root = FXMLLoader.load(getClass().getResource("FXMLdocument.fxml"));
+	    
 	    Scene scene = new Scene(root);
+	    //primaryStage.getIcons().add(new Image("javaicon.ico"));
 	    scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
+	    
+	    
 	    primaryStage.setTitle("DK SourceCode LineCounter");
+	  
+	    
+	   Image icon = new Image(getClass().getResourceAsStream("java_icon.png"));
+	    primaryStage.getIcons().add(icon);
 	    primaryStage.setScene(scene);
 	    primaryStage.show();
 	    primaryStage.setResizable(false);
